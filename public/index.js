99cbb1d00f510cdf9a786b8924b5b7c1cfc28ebf
chrisarap <carteaga460@gmail.com>
2022-11-12 22:41:17 -0400

    user and machine option received

--- public/index.js
+++ public/index.js
@@ -1,1 +1,9 @@
-console.log('hello world from js')
+
+const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const options = ['rock', 'paper', 'scissors'];
+
+const result = () => options[random(0,3)];
+
+module.exports={ result}; 
+
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const {result} = require('./public/index.js')
+
+
 
 app.use('/public', express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: false}));
@@ -9,7 +12,11 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
 
 app.post('/', (req, res) => {
-  console.log(req.body)
+  let userOption = req.body.option;
+
+  console.log(userOption, result());
+
+  res.redirect('/');
 });
 
 app.listen(port, () => console.log(`server listening in ${port}`));
