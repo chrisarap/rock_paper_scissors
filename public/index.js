0d042b698f2732bd4b4c81d0e9ae4c3a611db3b7
chrisarap <carteaga460@gmail.com>
2022-11-13 14:30:39 -0400

    basic interface created

--- public/index.js
+++ public/index.js
@@ -7,15 +7,31 @@ const result = () => options[random(0,3)];
 
 const operation = (player, machine) => {
    
-  const playerMessage = `player won ${player} > ${machine}`;
+  let winner = '';
+  let message = '';
 
-  if(player == machine) return 'draw';
-  
-  if(player == 'rock' && machine == 'scissors') return playerMessage;
-  if(player == 'scissors' && machine == 'paper') return playerMessage;
-  if(player == 'paper' && machine == 'rock') return playerMessage; 
+  let playerMessage = `player won ${player} > ${machine}`;
+  let machineMessage = `machine won ${machine} > ${player}`;
+ 
+  if(player == machine) {
+    winner = 'draw',
+    message = 'draw'
+   } else  if(
+    player == 'rock'     && machine == 'scissors' ||
+    player == 'scissors' && machine == 'paper'    ||
+    player == 'paper'    && machine == 'rock'     
+  ){
+    winner = 'player';
+    message = playerMessage;
+  } else {
 
-  return `machine won ${machine} > ${player}`;
+    winner = 'machine';
+    message = machineMessage;
+  }
+  return {
+    winner: winner,
+    message: message
+  }
 
 };
 
--- server.js
+++ server.js
@@ -3,16 +3,52 @@ const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const {result, operation} = require('./public/index.js')
+ 
+let count = {
+    player: 0,
+    machine: 0,
+    draw: 0
+  };
+
+let data = {
+  playerOption: '',
+  machineOption: '',
+  winner: '',
+  message: ''
+}
 
 app.use('/public', express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engione', 'pug');
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/views/index.html'));
+
+app.get('/', (req, res) => res.render(
+  __dirname + '/views/index.pug',
+  {
+    count: count,
+    data: data
+  }
+));
 
 app.post('/', (req, res) => {
+
   let userOption = req.body.option;
   let machineOption = result();
-  console.log(userOption, machineOption, operation(userOption, machineOption));
+  let answer= operation(userOption, machineOption);
+  
+  if(answer.winner == 'player')  count.player++;
+  if(answer.winner == 'machine') count.machine++;
+  if(answer.winner == 'draw')    count.draw++;
+
+  data.playerOption = userOption;
+  data.machineOption = machineOption;
+  data.winner = answer.winner;
+  data.message = answer.message;
+
+
+  console.log(userOption, machineOption, answer.message, count);
+
+
 
   res.redirect('/');
 });
